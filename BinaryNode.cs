863bcae3114ef3ef06374f0b5efbef1f55fe4e65
patrickwregan <pwr888@gmail.com>
2022-02-25 14:07:58 -0500

    project 1 milestone 1

---
+++ BinaryNode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_node1
+{
+    class BinaryNode<T>
+    {
+        public T Value { get; set; }
+        public BinaryNode<T> LeftChild;
+        public BinaryNode<T> RightChild;
+        public BinaryNode(T val)
+        {
+            Value = val;
+            LeftChild = null;
+            RightChild = null;
+        }
+
+        public void AddLeft(BinaryNode<T> child)
+        {
+            LeftChild = child;
+        }
+
+        public void AddRight(BinaryNode<T> child)
+        {
+            RightChild = child;
+        }
+
+        public override string ToString()
+        {
+            
+            return Value.ToString() + ": " + (LeftChild?.Value.ToString() ?? "null") + " " + (RightChild?.Value.ToString() ?? "null");
+        }
+
+    }
+}
---
+++ NaryNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace narynode1
+{
+    class NaryNode<T>
+    {
+        T Value { get; set; }
+        public List<NaryNode<T>> Children;
+
+        public NaryNode(T val)
+        {
+            Value = val;
+            Children = new List<NaryNode<T>>();
+        }
+
+        public void AddChild(NaryNode<T> node)
+        {
+            Children.Add(node);
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach(var v in Children)
+            {
+                result += v.ToString() + " ";
+            }
+            return Value.ToString() + ": " + result;
+        }
+
+
+
+    }
+
+
+}
