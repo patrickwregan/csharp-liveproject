1841374e48d4e5f6f6dd698ca2e397e58d168a05
patrickwregan <pwr888@gmail.com>
2022-03-08 11:13:32 -0500

    project 1 milestone 4

--- BinaryNode.cs
+++ BinaryNode.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace binary_node1
+namespace binarynode3
 {
     class BinaryNode<T>
     {
@@ -30,8 +30,181 @@ namespace binary_node1
 
         public override string ToString()
         {
-            
-            return Value.ToString() + ": " + (LeftChild?.Value.ToString() ?? "null") + " " + (RightChild?.Value.ToString() ?? "null");
+            return ToString("");
+
+            //return Value.ToString() + ": " + (LeftChild?.Value.ToString() ?? "null") + " " + (RightChild?.Value.ToString() ?? "null");
+        }
+
+        public string ToString(string spaces)
+        {
+            string result = spaces + Value + ":\n";
+
+            if(LeftChild != null || RightChild != null)
+            {
+                if(LeftChild == null)
+                {
+                    
+                    result += spaces + "    null" + "\n";
+                }
+
+                else
+                {
+                    
+                    result += LeftChild.ToString(spaces + "    ");
+                }
+
+                if(RightChild == null)
+                {
+                    result += spaces + "    null" + "\n";
+                }
+                else
+                {
+                    result += RightChild.ToString(spaces + "    ");
+                }
+            }
+            return result;
+        }
+
+        public BinaryNode<T> FindNode(T val)
+        {
+            //Console.WriteLine(Value.ToString()) ;
+            //BinaryNode<T> node;
+            if (EqualityComparer<T>.Default.Equals(Value, val))
+            {
+                return this;
+            }
+
+            if(LeftChild != null)
+            {
+                // recursive call either finds something (returns node or returns null
+                // if it does find something, we want to return that value but
+                // if it does not find anything we want to make sure to continue with 
+                // the right node so child if != null then return node (end recursive calls) otherwise
+                // continue to next if statement which does the same for RightNode
+                // I think this is slightly different than when you send the node as a 
+                // parameter of the function.
+                BinaryNode<T> node = LeftChild.FindNode(val);
+                if(node != null)
+                {
+                    return node;
+                }
+            }
+            if(RightChild != null)
+            {
+                BinaryNode<T> node = RightChild.FindNode(val);
+                if(node != null)
+                {
+                    return node;
+                }
+            }
+            return null;                 
+        }
+
+
+        internal List<BinaryNode<T>> TraversePreorder()
+        {
+            List<BinaryNode<T>> result = new List<BinaryNode<T>>();
+
+            // Add this node to the traversal.
+            result.Add(this);
+
+            // Add the child subtrees.
+            if (LeftChild != null)
+            {
+                result.AddRange(LeftChild.TraversePreorder());
+               
+            }
+            if (RightChild != null)
+            {
+                result.AddRange(RightChild.TraversePreorder());
+                
+                
+            }
+            return result;
+        }
+        
+        internal List<BinaryNode<T>> TraverseInorder()
+        {
+            List<BinaryNode<T>> result = new List<BinaryNode<T>>();
+
+            // Add the left subtree.
+            if (LeftChild != null)
+            {
+                result.AddRange(LeftChild.TraverseInorder());
+
+            }
+
+            // Add this node.
+            result.Add(this);
+
+            // Add the right subtree.
+            if (RightChild != null)
+            {
+                result.AddRange(RightChild.TraverseInorder());
+            }
+            return result;
+        }
+        
+        internal List<BinaryNode<T>> TraversePostorder()
+        {
+            List<BinaryNode<T>> result = new List<BinaryNode<T>>();
+
+            // Add the child subtrees.
+            if (LeftChild != null)
+            {
+                result.AddRange(LeftChild.TraversePostorder());
+            }
+            if (RightChild != null)
+            {
+                result.AddRange(RightChild.TraversePostorder());
+            }
+
+            // Add this node.
+            result.Add(this);
+            return result;
+        }
+
+        
+        internal List<BinaryNode<T>> TraverseBreadthFirst()
+        {
+            List<BinaryNode<T>> result = new List<BinaryNode<T>>();
+            Queue<BinaryNode<T>> queue = new Queue<BinaryNode<T>>();
+
+            // Start with the top node in the queue.
+            queue.Enqueue(this);
+            while (queue.Count > 0)
+            {
+                // Remove the top node from the queue and
+                // add it to the result list.
+                BinaryNode<T> node = queue.Dequeue();
+                result.Add(node);
+
+                // Add the node's children to the queue.
+                if (node.LeftChild != null)
+                {
+                    queue.Enqueue(node.LeftChild);
+                    
+                }
+                if (node.RightChild != null)
+                {
+                    queue.Enqueue(node.RightChild);
+                    
+                }
+            }
+            return result;
+        }
+        
+        public static void FindValue(BinaryNode<T> startingNode, T target)
+        {
+            BinaryNode<T> node = startingNode.FindNode(target);
+            if(node == null)
+            {
+                Console.WriteLine(target.ToString() + " not found.");
+            }
+            else
+            {
+                Console.WriteLine(target.ToString() + " found.");
+            }
         }
 
     }
--- NaryNode.cs
+++ NaryNode.cs
@@ -8,7 +8,7 @@ namespace narynode3
 {
     class NaryNode<T>
     {
-        T Value { get; set; }
+        public T Value { get; set; }
         public List<NaryNode<T>> Children;
 
         public NaryNode(T val)
@@ -51,12 +51,68 @@ namespace narynode3
                     return node;
                 }
             }
- 
-              
-            
             return null;
-
         }
+
+        internal List<NaryNode<T>> TraversePreorder()
+        {
+            List<NaryNode<T>> result = new List<NaryNode<T>>();
+
+            // Add this node to the traversal.
+            result.Add(this);
+
+            // Add the children.
+            foreach (NaryNode<T> child in Children)
+            {
+                result.AddRange(child.TraversePreorder());
+            }
+
+            return result;
+        }
+
+        internal List<NaryNode<T>> TraversePostorder()
+        {
+            List<NaryNode<T>> result = new List<NaryNode<T>>();
+
+            // Add the children.
+            foreach (NaryNode<T> child in Children)
+            {
+                result.AddRange(child.TraversePostorder());
+            }
+
+            // Add this node to the traversal.
+            result.Add(this);
+            return result;
+        }
+
+        internal List<NaryNode<T>> TraverseBreadthFirst()
+        {
+            List<NaryNode<T>> result = new List<NaryNode<T>>();
+            Queue<NaryNode<T>> queue = new Queue<NaryNode<T>>();
+
+            // Start with the top node in the queue.
+            queue.Enqueue(this);
+            while (queue.Count > 0)
+            {
+                // Remove the top node from the queue and
+                // add it to the result list.
+                NaryNode<T> node = queue.Dequeue();
+                result.Add(node);
+
+                // Add the node's children to the queue.
+                foreach(NaryNode<T> child in node.Children)
+                {
+                    if(child != null)
+                    {
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
         public static void FindValue(NaryNode<string> root, string target)
         {
             NaryNode<string> node = root.FindNode(target);
@@ -66,6 +122,12 @@ namespace narynode3
                 Console.WriteLine(string.Format("Found {0}", node.Value));
         }
 
+
+
+
+
     }
+
+
 
 }
